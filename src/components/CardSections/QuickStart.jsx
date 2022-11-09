a9517c2a2dc6fde9423137abf36d3c9f5d2d422d
debie <debbs.ly@gmail.com>
2022-11-09 09:02:26 -0800

    Update intro and quickstart title

--- src/components/CardSections/QuickStart.jsx
+++ src/components/CardSections/QuickStart.jsx
@@ -8,17 +8,17 @@ export default function QuickStartSection({ title, className }) {
   return (
     <Section title={title} className={className}>
       <Card
-        title="👩‍💻 Installing the Lit JavaScript SDK"
+        title="👩‍💻 Install the Lit JavaScript SDK"
         description="Set up the SDK within your local environment."
         to="/SDK/Explanation/installation"
       />
       <Card
-        title="👋 Hello World"
+        title="👋 Hello World w/ Lit Actions"
         description="Create your first Lit-enabled application using Lit Actions."
         to="/SDK/Explanation/litActions#hello-world"
       />
       <Card
-        title="📁 Encrypting On-Chain Metadata (Polygon)"
+        title="📁 Encrypt On-Chain Metadata (Polygon)"
         description="Encrypt on-chain meta-data (an NFT description) using the Lit SDK."
         to="/toolsAndExamples/SDKExamples/onchainMetadata/introduction"
       />

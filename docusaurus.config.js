6cbbebda0f02efc1fac89d286d4bf8ad5085d5d0
debie <debbs.ly@gmail.com>
2023-07-13 10:34:49 -0400

    test on current

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
       ({
         docs: {
           breadcrumbs: false,
-          lastVersion: "2.0",
+          lastVersion: "current",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl: "https://github.com/LIT-Protocol/docs/edit/main/website/",
